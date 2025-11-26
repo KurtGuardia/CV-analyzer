69df3adf773473bc203e1b79388fc58412263583
Kurt Guardia <kurtguardia@gmail.com>
2025-11-26 17:14:56 +0400

    fix(home): Add / to the bg url

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -47,7 +47,7 @@ export default function Home() {
   }, [])
 
   return (
-    <main className="bg-[url('images/bg-main.svg')] bg-cover">
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
       <section className='main-section'>
         <div className='page-heading py-16'>
